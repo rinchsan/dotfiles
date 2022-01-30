16fa82bdca5e57b0478f91a2ea7796f8ea47d10a
Masaya Hayashi <mcdonalds.only@gmail.com>
2022-01-30 16:34:03 +0900

    update

--- etc/scripts/init.sh
+++ etc/scripts/init.sh
@@ -4,6 +4,12 @@ set -eu
 
 brew bundle install --file=${HOME}/dotfiles/.Brewfile
 
+gh release download --repo marcosnils/bin --pattern '*Darwin_x86*'
+mv bin*Darwin_x86* bin
+chmod +x bin
+mv bin /usr/local/bin/bin
+bin ensure
+
 echo 'Changing default shell to fish'
 echo "$(which fish)" | sudo tee -a /etc/shells
 chsh -s "$(which fish)"
