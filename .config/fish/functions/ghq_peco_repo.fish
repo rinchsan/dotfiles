9600cc84074afdba251aea2bdd4d4e8328a081f9
Masaya Hayashi <mcdonalds.only@gmail.com>
2022-06-11 18:34:19 +0900

    update

--- .config/fish/functions/ghq_peco_repo.fish
+++ .config/fish/functions/ghq_peco_repo.fish
@@ -1,7 +1,7 @@
 function ghq_peco_repo
   set selected_repository (ghq list | peco --query "$LBUFFER")
   if [ -n "$selected_repository" ]
-    cd $selected_repository
+    cd (ghq root)/$selected_repository
     commandline -f repaint
   end
 end
