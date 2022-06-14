47422b2dd22a2b781e6a20084c4ec2713ffffdd7
Masaya Hayashi <mcdonalds.only@gmail.com>
2022-06-14 13:23:31 +0900

    update

--- etc/scripts/init.sh
+++ etc/scripts/init.sh
@@ -17,3 +17,4 @@ chsh -s "$(which fish)"
 echo 'Changing key repeat settings'
 defaults write -g InitialKeyRepeat -int 12 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+defaults write com.apple.PowerChime ChimeOnNoHardware -bool true;killall PowerChime
