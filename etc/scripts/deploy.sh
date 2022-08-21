8c5e5ce355d3dd9fece33486807e6fb63aa6b401
Masaya Hayashi <mcdonalds.only@gmail.com>
2022-08-21 13:06:18 +0900

    update

--- etc/scripts/deploy.sh
+++ etc/scripts/deploy.sh
@@ -13,3 +13,9 @@ do
     SRC=$(cd "$(dirname ${FILE})" && pwd)/$(basename ${FILE})
     ln -snfv ${SRC} ${HOME}/${FILE}
 done
+
+defaults write -g InitialKeyRepeat -int 12 # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+defaults write com.apple.PowerChime ChimeOnNoHardware -bool true
+defaults write com.apple.finder ShowPathbar -bool true
+defaults write com.apple.finder ShowStatusBar -bool true
--- etc/scripts/init.sh
+++ etc/scripts/init.sh
@@ -13,10 +13,3 @@ bin ensure
 echo 'Changing default shell to fish'
 echo "$(which fish)" | sudo tee -a /etc/shells
 chsh -s "$(which fish)"
-
-echo 'Changing key repeat settings'
-defaults write -g InitialKeyRepeat -int 12 # normal minimum is 15 (225 ms)
-defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
-defaults write com.apple.PowerChime ChimeOnNoHardware -bool true;killall PowerChime
-defaults write com.apple.finder ShowPathbar -bool true
-defaults write com.apple.finder ShowStatusBar -bool true
