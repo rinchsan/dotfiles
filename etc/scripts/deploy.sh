971d2b0ef6b616c2f6429683eb0d31f8a54428b8
Masaya Hayashi <mcdonalds.only@gmail.com>
2022-08-21 13:22:49 +0900

    update

--- etc/scripts/deploy.sh
+++ etc/scripts/deploy.sh
@@ -13,12 +13,3 @@ do
     SRC=$(cd "$(dirname ${FILE})" && pwd)/$(basename ${FILE})
     ln -snfv ${SRC} ${HOME}/${FILE}
 done
-
-defaults write -g InitialKeyRepeat -int 12 # normal minimum is 15 (225 ms)
-defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
-defaults write com.apple.PowerChime ChimeOnNoHardware -bool true
-defaults write com.apple.finder ShowPathbar -bool true
-defaults write com.apple.finder ShowStatusBar -bool true
-defaults write -g AppleShowScrollBars -string "Always"
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool true
-defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
