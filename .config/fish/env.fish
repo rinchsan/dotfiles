f6e1f9847b0714ab3fd8dc09a489c3b1ec0041fd
Masaya Hayashi <mcdonalds.only@gmail.com>
2023-03-12 01:42:22 +0900

    update

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -6,4 +6,5 @@ set -gx PATH $PATH $HOME/google-cloud-sdk/bin
 set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_DATA_HOME $HOME/.local/share
 set -gx FZF_DEFAULT_COMMAND 'rg --files --follow --no-ignore-vcs --hidden -g \'!{**/node_modules/*,**/.git/*}\''
+set -gx FZF_DEFAULT_OPTS '--height 50% --reverse --border'
 
