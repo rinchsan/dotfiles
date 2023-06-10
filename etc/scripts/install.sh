bafceb3d15249cb7566aeaaff25f9477b15f380e
Masaya Hayashi <mcdonalds.only@gmail.com>
2023-06-10 20:32:23 +0900

    update

--- etc/scripts/install.sh
+++ etc/scripts/install.sh
@@ -6,6 +6,7 @@ cd "${HOME}"
 
 if ! type brew >/dev/null 2>&1; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    export PATH=$PATH:/opt/homebrew/bin
 fi
 
 if [ ! -d dotfiles ]; then
