dc02d5895ec9a374c06d7682deb1bfebc2c8b5f5
Masaya Hayashi <mcdonalds.only@gmail.com>
2023-06-10 20:18:43 +0900

    update

--- etc/scripts/install.sh
+++ etc/scripts/install.sh
@@ -8,10 +8,6 @@ if ! type brew >/dev/null 2>&1; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-if ! type git >/dev/null 2>&1; then
-    brew install git
-fi
-
 if [ ! -d dotfiles ]; then
     git clone https://github.com/rinchsan/dotfiles.git
 fi
