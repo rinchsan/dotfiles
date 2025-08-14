3ec4c498225c42a1b6f1548b58acbfda19a2ca1c
Masaya Hayashi <mcdonalds.only@gmail.com>
2025-08-14 16:07:43 +0900

    update

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -4,6 +4,7 @@ set -gx PATH $PATH $GOPATH/bin
 set -gx PATH $PATH /flutter/bin
 set -gx PATH $PATH $HOME/google-cloud-sdk/bin
 set -gx PATH $PATH /opt/homebrew/bin
+set -gx PATH $PATH $HOME/.asdf/shims
 set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_DATA_HOME $HOME/.local/share
 set -gx FZF_DEFAULT_COMMAND 'rg --files --follow --no-ignore-vcs --hidden -g \'!{**/node_modules/*,**/.git/*}\''
