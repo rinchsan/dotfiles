44b7dda6c6c45efe4f47079ea5ff006210818fea
Masaya Hayashi <mcdonalds.only@gmail.com>
2026-05-03 20:40:13 +0900

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,3 +10,7 @@ if test -e ~/.config/fish/ignored.fish
 end
 
 string match -q "$TERM_PROGRAM" "kiro" and . (kiro --locate-shell-integration-path fish)
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
